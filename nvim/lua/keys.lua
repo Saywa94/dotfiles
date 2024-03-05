dc4f79739b5ac3108f7cedbf9b522aebd6ace368
Saywa94 <saywachavez@gmail.com>
2024-03-04 23:55:09 -0400

    Binded C-e to open harpoon directly, using telescope is too slow

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -50,6 +50,7 @@ map("n", "<leader>Y", [["+Y]], {})
 ------------------ Harpoon Shortcuts ---------------
 local harpoon = require('harpoon')
 harpoon:setup()
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 -- append current buffer to harpoon
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
