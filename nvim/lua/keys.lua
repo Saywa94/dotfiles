e66bfc81f4225ae668028bd7b2f2b921897a79d6
Saywa94 <saywachavez@gmail.com>
2024-03-30 12:41:23 -0400

    Added keymap to see al references to var in project on telescope

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -139,6 +139,7 @@ lsp_zero.on_attach(function(_, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
 	lsp_zero.default_keymaps({ buffer = bufnr })
+	vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = bufnr })
 end)
 
 local cmp = require("cmp")
