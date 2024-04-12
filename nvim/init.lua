afd1efea0476200a14602c90c3ad5384b379357e
Saywa94 <saywachavez@gmail.com>
2024-04-11 20:35:47 -0400

    Nvimtree: nvim closes when last buffer is closed even if nvimtree is open

--- nvim/init.lua
+++ nvim/init.lua
@@ -248,3 +248,19 @@ vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#7E8082" })
 require("Comment").setup()
 -- Todo highlight
 require("todo-comments").setup()
+
+-- Completly close nvim when nvimtree is still open
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+	pattern = "NvimTree_*",
+	callback = function()
+		local layout = vim.api.nvim_call_function("winlayout", {})
+		if
+			layout[1] == "leaf"
+			and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
+			and layout[3] == nil
+		then
+			vim.cmd("confirm quit")
+		end
+	end,
+})
